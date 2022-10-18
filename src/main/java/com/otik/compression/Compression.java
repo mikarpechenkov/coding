690dbf800648d54a474f9d81d7aa540dcecc2d8f
mikarpechenkov <98464767+mikarpechenkov@users.noreply.github.com>
2022-10-19 01:55:39 +0300

    Добавил комменты, чуть разобрался, что лучше использовать для хранения данных

--- src/main/java/com/otik/compression/Compression.java
+++ src/main/java/com/otik/compression/Compression.java
@@ -1,13 +1,31 @@
 package com.otik.compression;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class Compression<T extends Comparable<? super T>> {
+    /*
+    TreeMap нам не поможет. Оно по сути только для уменьшения времени работы с мэпами, в которых мы часто ищем.
+    Также для в них есть методы, которые позволяют делить карты,
+    но для того, чтобы их сортировать по значению (в данном случае нужно именно так) используются костыли,
+    поэтому решил, что LinkedHashMap подходит больше
+     */
     private Map<T, Double> probability = new LinkedHashMap<>();
-//    private Map<T,>
+    /*
+    В этой мэпе будут лежать:
+     ключи – значение байта и значения – код для конкретного байта.
+     TreeMap подходит лучше, потому что мы будем поэтапно формировать код для каждого байта,
+     следовательно, часто выполнять поиск по мэпе.
+     По этой же причине используем StringBuilder, он менее ресурсоемкий, чем String, когда необходимо часто изменять строку
+     Кроме того, к этой же мэпе будем обращаться и при дешифровке.
+     На порядок элементов нам все равно, мы ее не сортируем.
+     */
+    private TreeMap<T,StringBuilder> codes=new TreeMap<>();
 
     public Compression(Map<T, Double> probability) {
         this.probability = probability;
@@ -23,13 +41,16 @@ public class Compression<T extends Comparable<? super T>> {
                         LinkedHashMap::new));
     }
 
-    private void defineCiphers() {
-        Double sumProbabilityOfFirstPart = 0.0; //которая стоит слева
+    //Думаю лекче всего сделать рекурсией
+    private void defineCiphers(@NotNull Map<T,Double> map) {
+        Double sumProbabilityOfFirstPart = 0.0; //которая стоит справа, где 1
         Double sumProbabilityOfSecondPart = 0.0;
-        Iterator<Map.Entry<T, Double>> firstIterator = probability.entrySet().iterator();
-        Iterator<Map.Entry<T, Double>> secondIterator = probability.entrySet().iterator();
-        while (sumProbabilityOfFirstPart <= sumProbabilityOfSecondPart) {
+        Iterator<Map.Entry<T, Double>> firstIterator = map.entrySet().iterator();
+        Iterator<Map.Entry<T, Double>> secondIterator = map.entrySet().iterator();
+        if(map.size()!=1){
+            while (sumProbabilityOfFirstPart <= sumProbabilityOfSecondPart) {
 
+            }
         }
     }
 }
