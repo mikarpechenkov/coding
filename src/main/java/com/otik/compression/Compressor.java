3c00593e5def6fab51ecd6666aed8e44bb24f57b
mikarpechenkov <98464767+mikarpechenkov@users.noreply.github.com>
2022-10-19 21:01:23 +0300

    Стало лучше, но все равно не оптимальный код строит

--- src/main/java/com/otik/compression/Compressor.java
+++ src/main/java/com/otik/compression/Compressor.java
@@ -12,7 +12,7 @@ public class Compressor<T extends Comparable<? super T>> {
     но для того, чтобы их сортировать по значению (в данном случае нужно именно так) используются костыли,
     поэтому решил, что LinkedHashMap подходит больше
      */
-    private Map<T, Double> probability = new LinkedHashMap<>();
+    private Map<T, Integer> repetition = new LinkedHashMap<>();
     /*
     В этой мэпе будут лежать:
      ключи – значение байта и значения – код для конкретного байта.
@@ -24,20 +24,20 @@ public class Compressor<T extends Comparable<? super T>> {
      */
     private TreeMap<T, StringBuilder> codes = new TreeMap<>();
 
-    public Compressor(Map<T, Double> probability) {
-        this.probability = probability;
-        this.probability.forEach((k, v) -> codes.put(k, new StringBuilder("")));
+    public Compressor(Map<T, Integer> repetition) {
+        this.repetition = repetition;
+        this.repetition.forEach((k, v) -> codes.put(k, new StringBuilder("")));
     }
 
     public TreeMap<T, StringBuilder> getCodes() {
         sortProbability();
-        defineCiphers(probability);
+        defineCiphers(repetition);
         return codes;
     }
 
     private void sortProbability() {
-        probability = probability.entrySet().stream()
-                .sorted(Map.Entry.<T, Double>comparingByValue().reversed())
+        repetition = repetition.entrySet().stream()
+                .sorted(Map.Entry.<T, Integer>comparingByValue())
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
                         (k, v) -> {
                             throw new AssertionError();
@@ -46,17 +46,17 @@ public class Compressor<T extends Comparable<? super T>> {
     }
 
 
-    private void defineCiphers(@NotNull Map<T, Double> map) {
+    private void defineCiphers(@NotNull Map<T, Integer> map) {
         if (map.size() != 1) {
             double mediumProbability = 0.0;
             double sumProbabilityOfFirstPart = 0.0; //которая стоит справа, где 1
-            Map<T, Double> firstPart = new LinkedHashMap<>();
-            Map<T, Double> secondPart = new LinkedHashMap<>();
+            Map<T, Integer> firstPart = new LinkedHashMap<>();
+            Map<T, Integer> secondPart = new LinkedHashMap<>();
 
             for (double value : map.values())
                 mediumProbability += (value / 2);
 
-            for (Map.Entry<T, Double> entry : map.entrySet())
+            for (Map.Entry<T, Integer> entry : map.entrySet())
                 if (sumProbabilityOfFirstPart < mediumProbability) {
                     firstPart.put(entry.getKey(), entry.getValue());
                     codes.get(entry.getKey()).append("1");
@@ -68,6 +68,5 @@ public class Compressor<T extends Comparable<? super T>> {
             defineCiphers(firstPart);
             defineCiphers(secondPart);
         }
-
     }
 }
